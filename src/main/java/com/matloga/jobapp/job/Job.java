66438338a6e47392977a28fb04d63d18be20aae8
Tsumbedzo Matloga <tsumbedzomatloga@gmail.com>
2026-06-19 15:54:07 +0200

    Initial commit

---
+++ src/main/java/com/matloga/jobapp/job/Job.java
@@ -0,0 +1,88 @@
+package com.matloga.jobapp.job;
+
+import com.matloga.jobapp.company.Company;
+import jakarta.persistence.*;
+
+@Entity
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tittle;
+    private String description;
+    private String minSalary;
+    private String maxSalary;
+    private String location;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    public Job() {
+    }
+
+    public Job(Long id, String tittle, String description, String minSalary, String maxSalary, String location) {
+        this.id = id;
+        this.tittle = tittle;
+        this.description = description;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        this.location = location;
+    }
+
+    public Company getCompanies() {
+        return company;
+    }
+
+    public void setCompanies(Company company) {
+        this.company = company;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(String minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public String getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(String maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
